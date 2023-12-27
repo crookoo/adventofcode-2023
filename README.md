4936e72437f5b7675f40b7c5d91e949c275c34c6
Michael Zimmermann <info@michaelzimmermann.com>
2023-12-27 20:53:50 +0100

    day09

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Example: ```node 1``` to execute 1.js in the same directory.
 | Day  | Part One | Part Two |
 |---|:---:|:---:|
 | [Day 1: Trebuchet?!](https://github.com/crookoo/adventofcode-2023/tree/main/day01)| ⭐ | ⭐ |
+| [Day 2: Cube Conundrum](https://github.com/crookoo/adventofcode-2023/tree/main/day02)| ⭐ | ⭐ |
+| [Day 3: Gear Ratios](https://github.com/crookoo/adventofcode-2023/tree/main/day03)| ⭐ | ⭐ |
+| [Day 4: Scratchcards](https://github.com/crookoo/adventofcode-2023/tree/main/day04)| ⭐ | ⭐ |
+| [Day 5: If You Give A Seed A Fertilizer](https://github.com/crookoo/adventofcode-2023/tree/main/day05)| ⭐ ||
+| [Day 6: Wait For It](https://github.com/crookoo/adventofcode-2023/tree/main/day06)| ⭐ | ⭐ |
+| [Day 7: Camel Cards](https://github.com/crookoo/adventofcode-2023/tree/main/day07)| ⭐ | ⭐ |
+| [Day 8: Haunted Wasteland](https://github.com/crookoo/adventofcode-2023/tree/main/day08)| ⭐ ||
+| [Day 9: Mirage Maintenance](https://github.com/crookoo/adventofcode-2023/tree/main/day09)| ⭐ | ⭐ |
--- day09/1.js
+++ day09/1.js
@@ -1,4 +1,47 @@
 import { readInputFromFile } from '../inputReader/inputReader.js';
 
-const rawInput = readInputFromFile('input-test', import.meta.url);
-const input = rawInput;
+const rawInput = readInputFromFile('input', import.meta.url);
+const input = rawInput
+    .split('\n')
+    .map(line => line.split(' ').map(Number));
+
+
+let sumNextValues = 0;
+let sumPreviousValues = 0;
+
+input.forEach(history => {
+    let historyCalculations = [];
+    historyCalculations.push(history);
+    while (!allElementsAreZeros(history)) {
+        let nextLine = [];
+        for (let i = 0; i < history.length - 1; i++) {
+            let difference = history[i + 1] - history[i];
+            nextLine.push(difference);
+        }
+        historyCalculations.push(nextLine);
+        history = nextLine;
+    }
+
+    historyCalculations.forEach(calculationLine => {
+        sumNextValues += calculationLine[calculationLine.length - 1];
+    })
+
+    let firstValue = 0;
+    for (let i = historyCalculations.length - 2; i >= 0; i--) {
+        firstValue = historyCalculations[i][0] - firstValue;
+    }
+    sumPreviousValues += firstValue;
+})
+
+function allElementsAreZeros(arrayWithNumbers) {
+    for (let i = 0; i < arrayWithNumbers.length; i++) {
+        if (arrayWithNumbers[i] !== 0) return false;
+    }
+    return true;
+}
+
+console.log(sumNextValues);
+// 1743490457 correct
+
+console.log(sumPreviousValues);
+// 1053 correct
